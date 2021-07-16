f88c7ce62a01c0bb99ae41345f57df7c2e5ce228
unknown <s6263269@gmail.com>
2021-07-16 21:02:17 +0800

    add backend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,24 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { Container } from 'react-bootstrap';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
 
 const App = () => {
   return (
-    <>
+    <Router>
       <Header />
       <main className='py-3'>
         <Container>
-          <HomeScreen />
+          <Route path='/' component={HomeScreen} exact />
+          {/* 沒有  exact path /後面接甚麼都可以還是會到 HomeScreen*/}
+          <Route path='/product/:id' component={ProductScreen} exact />
         </Container>
       </main>
       <Footer />
-    </>
+    </Router>
   );
 };
 
