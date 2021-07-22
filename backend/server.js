1539ec3b46d028656691b73a1b2c69c5936cf73c
unknown <s6263269@gmail.com>
2021-07-22 18:06:54 +0800

    Prepare for deploy

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from 'path';
 import express from 'express';
 import dotenv from 'dotenv';
 //如果要引用ES module  file 引用要變成 .js
@@ -11,11 +12,20 @@ connectDB();
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('API is running..');
-});
-
 app.use('/api/products', productRoutes);
+
+const __dirname = path.resolve();
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/build')));
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  );
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running..');
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 
